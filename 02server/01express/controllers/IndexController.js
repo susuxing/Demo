5ac92d1f062258cb7e5aeb573534f22d3356e792
ajiu-hexiaohong <3050757636@qq.com>
2019-09-11 15:32:24 +0800

    add the code which is wx

--- 02server/01express/controllers/IndexController.js
+++
@@ -1,8 +0,0 @@
-let IndexService = require("../services/IndexService");
-module.exports.index = function(req,res){
-    let indexService = new IndexService();
-    indexService.getData(function(ob){
-        res.json(ob);
-    });
-};
-
